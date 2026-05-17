65262aa96d66a7eee873582c3c1e05ee58ce9027
TomasB <to.blanchard@duocuc.cl>
2026-05-17 18:51:38 -0400

    Update DataLoader.java

--- src/main/java/cl/duoc/ms_usuarios/config/DataLoader.java
+++ src/main/java/cl/duoc/ms_usuarios/config/DataLoader.java
@@ -2,9 +2,10 @@ package cl.duoc.ms_usuarios.config;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
-import cl.duoc.ms_usuarios.Model.Perfil;
-import cl.duoc.ms_usuarios.Repository.JuegoFavoritoRepository;
-import cl.duoc.ms_usuarios.Repository.PerfilRepository;
+
+import cl.duoc.ms_usuarios.model.Perfil;
+import cl.duoc.ms_usuarios.repository.JuegoFavoritoRepository;
+import cl.duoc.ms_usuarios.repository.PerfilRepository;
 
 @Configuration
 public class DataLoader {
